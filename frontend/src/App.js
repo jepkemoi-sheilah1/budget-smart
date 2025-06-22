1d4d79c6dc2b6cc0eaad8c25d30adc2711c88b04
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-06-22 13:33:52 +0300

    upadated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
